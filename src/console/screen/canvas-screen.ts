c990f3da7f60e4f01e01aa4223dd16c0d808538b
John <dmt.chmk@gmail.com>
2026-05-06 13:49:18 +0300

    Set canvas scaling using css

--- src/console/screen/canvas-screen.ts
+++ src/console/screen/canvas-screen.ts
@@ -4,33 +4,16 @@ const WIDTH = 256;
 const HEIGHT = 240;
 
 export class CanvasScreen implements Screen {
-  private width: number;
-
-  private height: number;
-
   private ctx: CanvasRenderingContext2D;
 
-  private hCanvas: HTMLCanvasElement;
-
-  private hctx: CanvasRenderingContext2D;
-
-  private hImageData: ImageData;
+  private imageData: ImageData;
 
   private dataView: DataView;
 
   constructor(canvas: HTMLCanvasElement) {
-    this.width = canvas.width;
-    this.height = canvas.height;
     this.ctx = canvas.getContext('2d', { alpha: false })!;
-    this.ctx.imageSmoothingEnabled = true;
-    this.ctx.imageSmoothingQuality = 'medium';
-
-    this.hCanvas = document.createElement('canvas') as HTMLCanvasElement;
-    this.hCanvas.width = WIDTH;
-    this.hCanvas.height = HEIGHT;
-    this.hctx = this.hCanvas.getContext('2d', { alpha: false })!;
-    this.hImageData = this.hctx.createImageData(WIDTH, HEIGHT);
-    this.dataView = new DataView(this.hImageData.data.buffer);
+    this.imageData = this.ctx.createImageData(WIDTH, HEIGHT);
+    this.dataView = new DataView(this.imageData.data.buffer);
   }
 
   setPixel(x: number, y: number, color: number): void {
@@ -38,11 +21,10 @@ export class CanvasScreen implements Screen {
   }
 
   update(): void {
-    this.hctx.putImageData(this.hImageData, 0, 0);
-    this.ctx.drawImage(this.hCanvas, 0, 0, this.width, this.height);
+    this.ctx.putImageData(this.imageData, 0, 0);
   }
 
   clear(): void {
-    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.clearRect(0, 0, WIDTH, HEIGHT);
   }
 }
